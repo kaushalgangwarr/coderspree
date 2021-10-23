9d80e11d84fe3b218efd3c26ab02bb30896f7b02
kaushalgangwarr <kaushalg20dec@gmail.com>
2021-10-23 14:59:54 +0530

    Create PrintAllPrimesTilln.cpp

---
+++ AR-VR/kaushalgangwarr_KaushalGangwar_2024cse1077_2/GettingStarted/PrintAllPrimesTilln.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int low, high;
+    int i,j;
+    scanf("%d%d",&low,&high);
+    for(i=low;i<=high;i++)
+    {
+        int count=0;
+       for(j=2;j<=high;j++)
+       {
+           if(i%j==0)
+           {
+           count++;
+           }
+       }
+       if(count==1)
+       {
+           printf("%d\n",i);
+       }
+    }
+    return 0;
+}
